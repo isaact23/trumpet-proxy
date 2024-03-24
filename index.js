3eb1ade192f81ce7dc2d95df6246267226e8f05a
Isaac Thompson <shufflesninja@gmail.com>
2024-03-24 06:43:39 -0400

    millis

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ app.use(express.static(PATH));
 let img = null;
 let notes = {
   0: {
-    time: 1,
+    time: 1000,
     val1: 1,
     val2: 0,
     val3: 0
   },
   1: {
-    time: 1,
+    time: 1000,
     val1: 0,
     val2: 1,
     val3: 0
   },
   2: {
-    time: 1,
+    time: 1000,
     val1: 0,
     val2: 0,
     val3: 1
